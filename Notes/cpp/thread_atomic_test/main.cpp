6c29ed2d1317cec26ff32476488138e15c4b7048
shines77 <gz_shines@msn.com>
2017-04-07 18:15:12 +0800

    更新了/thread_atomic_test/main.cpp;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- Notes/cpp/thread_atomic_test/main.cpp
+++ Notes/cpp/thread_atomic_test/main.cpp
@@ -29,14 +29,13 @@ struct Task {
     bool isCanceled() const { return canceled; }
 };
 
-void test_thread_proc(const Task & task, uint32_t hardware_concurrency)
+void thread_test_proc(const Task & task, uint32_t hardware_concurrency)
 {
-    uint32_t tls_counter;
     if (!task.isCanceled()) {
         std::cout << ">> [" << task.index << "]" << "send control command." << std::endl;
 
-        //tls_counter = g_counter.fetch_add(1) + 1;
-        tls_counter = ++g_counter;
+        //uint32_t tls_counter = g_counter.fetch_add(1) + 1;
+        uint32_t tls_counter = ++g_counter;
         std::ostringstream oss;
         oss << ">> index: " << task.index << ", counter: " << tls_counter << std::endl;
         std::cout << oss.str().c_str() << std::endl;
@@ -50,13 +49,13 @@ void test_thread_proc(const Task & task, uint32_t hardware_concurrency)
     }
 }
 
-void atomic_test(uint32_t thread_num)
+void thread_atomic_test(uint32_t thread_num)
 {
     std::unique_ptr<Task[]> tasks(new Task[thread_num]());
     std::unique_ptr<std::unique_ptr<std::thread>[]>
         threads(new std::unique_ptr<std::thread>[thread_num]());
 
-    std::cout << "--------------------- atomic_test() -----------------------" << std::endl << std::endl;
+    std::cout << "--------------------- thread_atomic_test() -----------------------" << std::endl << std::endl;
 
     {
         for (int i = 0; i < (int)thread_num; ++i) {
@@ -65,10 +64,10 @@ void atomic_test(uint32_t thread_num)
             assert(pTask1 == pTask2);
 #if 1
             tasks[i].index = i + 1;
-            threads[i].reset(new std::thread(test_thread_proc, tasks[i], thread_num));
+            threads[i].reset(new std::thread(thread_test_proc, tasks[i], thread_num));
 #else
             tasks.get()[i].index = i + 1;
-            threads.get()[i].reset(new std::thread(test_thread_proc, tasks.get()[i], thread_num));
+            threads.get()[i].reset(new std::thread(thread_test_proc, tasks.get()[i], thread_num));
 #endif
         }
 
@@ -82,7 +81,7 @@ void atomic_test(uint32_t thread_num)
 
     std::cout << ">>>> All " << thread_num << " thread(s) run finished." << std::endl << std::endl;
 
-    std::cout << "-----------------------------------------------------------" << std::endl << std::endl;
+    std::cout << "-----------------------------------------------------------------" << std::endl << std::endl;
 }
 
 template <typename T>
@@ -92,7 +91,7 @@ std::shared_ptr<T> make_shared_array(size_t size)
     return std::shared_ptr<T>(new T[size](), std::default_delete<T[]>());
 }
 
-void atomic_test2(uint32_t thread_num)
+void thread_atomic_test2(uint32_t thread_num)
 {
 #if 1
     std::shared_ptr<Task> tasks = make_shared_array<Task>(thread_num);
@@ -105,12 +104,12 @@ void atomic_test2(uint32_t thread_num)
                 [](std::shared_ptr<std::thread> * p) { delete[] p; });
 #endif
 
-    std::cout << "--------------------- atomic_test2() ----------------------" << std::endl << std::endl;
+    std::cout << "--------------------- thread_atomic_test2() ----------------------" << std::endl << std::endl;
 
     {
         for (int i = 0; i < (int)thread_num; ++i) {
             tasks.get()[i].index = i + 1;
-            threads.get()[i].reset(new std::thread(test_thread_proc, tasks.get()[i], thread_num));
+            threads.get()[i].reset(new std::thread(thread_test_proc, tasks.get()[i], thread_num));
         }
 
         std::cout << ">>>> All " << thread_num << " thread(s) create done." << std::endl << std::endl;
@@ -123,13 +122,14 @@ void atomic_test2(uint32_t thread_num)
 
     std::cout << ">>>> All " << thread_num << " thread(s) run finished." << std::endl << std::endl;
 
-    std::cout << "-----------------------------------------------------------" << std::endl << std::endl;
+    std::cout << "-----------------------------------------------------------------" << std::endl << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
-    atomic_test(max_thread_cnt);
-    atomic_test2(max_thread_cnt);
+    thread_atomic_test(max_thread_cnt);
+    g_counter.store(0);
+    thread_atomic_test2(max_thread_cnt);
 
     ::system("pause");
     return 0;
